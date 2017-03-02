69ba3fc37e8e21a9f409f57414d5cd42e9818165
ColacX <DanielZheng1989@gmail.com>
2017-03-02 11:02:17 +0100

    changed target version to android api 21. and test that it works on galaxy nexus android version 5.0.1

--- Kerrigan.Android/app/src/main/java/com/symbio/kerrigan/MainActivity.java
+++ Kerrigan.Android/app/src/main/java/com/symbio/kerrigan/MainActivity.java
@@ -5,13 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 
 import com.crashlytics.android.Crashlytics;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import io.fabric.sdk.android.Fabric;
+
+class CustomWebViewClient extends WebViewClient
+{
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request)
+    {
+        String scheme = request.getUrl().getScheme();
+        String method = request.getMethod();
+
+        if (scheme.equals("app")) {
+            InputStream dataStream = new ByteArrayInputStream("app scheme button data".getBytes(StandardCharsets.UTF_8));
+            return new WebResourceResponse("text/html", "utf-8", dataStream);
+        }
+
+        if (request.getUrl().toString() == "app://home") {
+            //should load the app home page
+        }
+
+        if (request.getUrl().toString() == "app://settings") {
+
+        }
+
+        return super.shouldInterceptRequest(view, request);
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,20 +55,9 @@ public class MainActivity extends AppCompatActivity {
         final WebView webView = (WebView)findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new CustomWebViewClient());
 
         webView.loadUrl("file:///android_asset/web/page1.html");
-        //webView.loadDataWithBaseURL("file://", htmlData, "text/html", "utf-8", null);
-        //webView.loadUrl("https://google.com");
-
-        ImageButton imageButton = (ImageButton)findViewById(R.id.imageButton);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                webView.loadUrl("https://google.com");
-            }
-        });
     }
 
     public void forceCrash(View view) {
