16934bb246b947cbc720451d394a436f3d6c3101
ColacX <danielzheng1989@gmail.com>
2017-03-03 23:55:02 +0100

    extracted WebViewClient

---
+++ Kerrigan.Android/app/src/main/java/com/symbio/kerrigan/CustomWebViewClient.java
@@ -0,0 +1,39 @@
+package com.symbio.kerrigan;
+
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CustomWebViewClient extends WebViewClient
+{
+    private static final String TAG = "CustomWebViewClient";
+
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request)
+    {
+        String scheme = request.getUrl().getScheme();
+        String method = request.getMethod();
+        String url = request.getUrl().toString();
+        String urlHost = request.getUrl().getHost().toString();
+
+//        if (scheme.equals("app")) {
+//            InputStream dataStream = new ByteArrayInputStream("app scheme button data".getBytes(StandardCharsets.UTF_8));
+//            return new WebResourceResponse("text/html", "utf-8", dataStream);
+//        }
+
+        if (url.equals("app://home")) {
+            //should load the app home page
+        }
+
+        if (url.equals("app://settings")) {
+
+        }
+
+        if (url.equals("app://crash")) {
+            throw new RuntimeException("This is a forced runtime crash");
+        }
+
+        return super.shouldInterceptRequest(view, request);
+    }
+}
--- Kerrigan.Android/app/src/main/java/com/symbio/kerrigan/MainActivity.java
+++ Kerrigan.Android/app/src/main/java/com/symbio/kerrigan/MainActivity.java
@@ -23,39 +23,6 @@ import java.nio.charset.StandardCharsets;
 
 import io.fabric.sdk.android.Fabric;
 
-class CustomWebViewClient extends WebViewClient
-{
-    private static final String TAG = "CustomWebViewClient";
-
-    @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request)
-    {
-        String scheme = request.getUrl().getScheme();
-        String method = request.getMethod();
-        String url = request.getUrl().toString();
-        String urlHost = request.getUrl().getHost().toString();
-
-//        if (scheme.equals("app")) {
-//            InputStream dataStream = new ByteArrayInputStream("app scheme button data".getBytes(StandardCharsets.UTF_8));
-//            return new WebResourceResponse("text/html", "utf-8", dataStream);
-//        }
-
-        if (url.equals("app://home")) {
-            //should load the app home page
-        }
-
-        if (url.equals("app://settings")) {
-
-        }
-
-        if (url.equals("app://crash")) {
-            throw new RuntimeException("This is a forced runtime crash");
-        }
-
-        return super.shouldInterceptRequest(view, request);
-    }
-}
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
