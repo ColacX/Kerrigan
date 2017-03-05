2269819556298e9bc35bb0c34bbdaa5c7a5a596d
ColacX <danielzheng1989@gmail.com>
2017-03-05 02:04:41 +0100

    added redirect to url with push notifications

--- Kerrigan.Android/app/src/main/java/com/piratpartiet/kerrigan/MainActivity.java
+++ Kerrigan.Android/app/src/main/java/com/piratpartiet/kerrigan/MainActivity.java
@@ -1,5 +1,6 @@
 package com.piratpartiet.kerrigan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,19 +10,22 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.json.JSONObject;
 
 //the first activity that starts
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private String urlToLoad = "file:///android_asset/web/splash.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
 
-        //setup crash reporting
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+
+		//setup crash reporting
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread thread, Throwable ex) {
                 FirebaseCrash.report(ex);
@@ -41,12 +45,37 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "firebaseToken: " + FirebaseInstanceId.getInstance().getToken());
 
         //initialize web-view
-        final WebView webView = (WebView)findViewById(R.id.webView);
+        final WebView webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new CustomWebViewClient());
-        webView.loadUrl("file:///android_asset/web/splash.html");
 
+        CheckIntent();
+        webView.loadUrl(urlToLoad);
         //throw new RuntimeException("This is a forced runtime exception");
+    }
+
+    private void CheckIntent()
+    {
+        try{
+            Intent intent = getIntent();
+            if (intent == null) {
+                return;
+            }
+
+            String jsonData = intent.getStringExtra("jsonData");
+            if (jsonData == null || jsonData.isEmpty()) {
+                return;
+            }
+
+            JSONObject jsonObject = new JSONObject(jsonData);
+            String url = jsonObject.getString("url");
+            if(url != null && !url.isEmpty()){
+                urlToLoad = url;
+            }
+        }
+        catch (Throwable t){
+            FirebaseCrash.report(t);
+        }
     }
 }
