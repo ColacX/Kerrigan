c041bcdefb864af951e54eda39478f9b5cbc93a8
ColacX <danielzheng1989@gmail.com>
2017-05-25 03:00:34 +0200

    improved alarm page

--- Kerrigan.Android/app/src/main/assets/web/main.js
+++ Kerrigan.Android/app/src/main/assets/web/main.js
@@ -1,12 +1,15 @@
-function createSwitch(buttonText){
+function appendSwitch(buttonText){
 	var t = $("#switchTemplate").html();
 	t = t.replace(new RegExp("{{text}}", "g"), buttonText);
 	$(t).appendTo("body");
 }
 
 $("document").ready(function(){
-	createSwitch("Wash Face");
-	createSwitch("Brush Teeths");
+	appendSwitch("Wash Face");
+	appendSwitch("Brush Teeths");
+	appendSwitch("Shave");
+	appendSwitch("Fix Hair");
+	appendSwitch("Make Bed");
 	
 	var w = window.innerWidth;
 	var h = window.innerHeight;
@@ -14,7 +17,7 @@ $("document").ready(function(){
 	var player = $("#audioPlayer")[0];
 	player.src = "01-second-flight.mp3";
 	player.volume = 0.1
-	player.play();
+	//player.play();
 	//player.pause();
 	player.currentTime = 0;
 	console.log();
